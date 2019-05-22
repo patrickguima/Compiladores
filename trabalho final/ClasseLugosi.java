83299870660255a7bd6b38ddd026061a8cbb8914
jaderfroes <jfdsrosa@inf.ufpel.edu.br>
2019-05-22 16:24:13 -0300

    classes adicionadas

--- trabalho final/ClasseLugosi.java
+++ trabalho final/ClasseLugosi.java
@@ -22,14 +22,40 @@ class Atribuicao extends Comando{
 	String var;
 	Exp exp;
 }
-
 class ChamadaFuncao extends Comando{
 	String nomeFuncao;
 	ArrayList <Exp> argumentos;
 }
-
 class If extends Comando{
 	Exp exp;
 	ArrayList <Comando> then;
 }
+class While extends Comando{
+	Exp exp;
+	ArrayList <Comando> comandos;
+}
+class DoWhile extends Comando{
+	ArrayList <Comando> comandos;
+	Exp exp;
+}
+class Return extends Comando{
+	Exp exp;
+} 
+class Print extends Comando{
+	Exp exp;
+}
 
+class Exp{}
+class OperadorExp extends Exp{
+	Exp arg1;
+	Exp arg2;
+	String op;
+}
+class Fator extends Exp{}
+class TokenId extends Fator{
+	String id;
+}
+class ListaExp extends Fator{
+	String id;
+	ArrayList <Exp> listExp;
+}
